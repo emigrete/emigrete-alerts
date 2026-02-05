7e9d383c6782faba46df9cc3533ed4f0239c478a
Emigrete <teowelyczko@gmail.com>
2026-02-04 23:40:46 -0300

    Fix: Agregar X-User-Id a allowedHeaders en CORS

--- server/src/index.js
+++ server/src/index.js
@@ -34,7 +34,7 @@ app.use(cors({
   origin: FRONTEND_URL,
   methods: ['GET', 'POST', 'DELETE', 'PUT', 'OPTIONS'],
   credentials: true,
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-User-Id']
 }));
 
 app.use('/api/billing/webhook/mercadopago', express.raw({ type: 'application/json' }));
